83551aa5b1779bfaa70ca0704f492e3916903bcb
Samon03 <samon3123@gmail.com>
2020-11-23 20:39:55 +0600

    user prompt

--- src/Message.js
+++ src/Message.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import './Message.css';
 
-function Message(props) {
+import {Card, CardContent, Typography} from '@material-ui/core';
+
+function Message({message, username}) {
+    const isUser = username === message.username;
     return (
-        <div>
-           <h4>{props.username}: {props.text}</h4>
+        <div className={`message ${isUser && 'message__user'}`}>
+            <Card className={isUser ? "messange__userCard" : "message__guestCard"}>
+                <CardContent>
+                    <Typography 
+                    variant="h6" color="white" 
+                    component="h2">
+                        {message.username}: {message.text}
+                    </Typography>
+                </CardContent>
+            </Card>
         </div>
     )
 }
